9630829b4cf3a51f167e9a4cb994d75a7d032d87
Tim Gates <tim.gates@iress.com>
2019-07-07 07:42:07 +1000

    100% coverage of main module

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -19,6 +19,31 @@ def test_main():
     )
     with fake_docopt, pytest.raises(SystemExit) as excctxt:
         # Exercise
-        main()  # pylint: disable=assignment-from-no-return
+        main()
     # Verify
     assert excctxt.value.args[0] == 1  # nosec
+
+
+def test_main_junit():
+    """
+    GIVEN the pipefish.__main__ module entry point WHEN calling
+    main selecting junit processing THEN the call processes the sample
+    junit file.
+    """
+    # Setup
+    from pipefish.__main__ import main
+    import mock
+    samplepath = 'samplepath'
+    fake_docopt = mock.patch(
+        'pipefish.__main__.docopt', return_value={
+            '--junit': samplepath
+        }
+    )
+    fake_process = mock.patch(
+        'pipefish.__main__.process_junit_xml', return_value='',
+    )
+    with fake_docopt, fake_process as mock_process:
+        # Exercise
+        main()
+    # Verify
+    mock_process.assert_called_with(samplepath)
