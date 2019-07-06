0128c96411f13801fbbda90795cc5c8d8bbba56a
Tim Gates <tim.gates@iress.com>
2019-07-07 08:36:01 +1000

    Resolve pylint issues

--- app/pipefish/process_junit.py
+++ app/pipefish/process_junit.py
@@ -2,7 +2,13 @@
 Utility functions for processing JUnit XML reports
 """
 
+import collections
+
 import defusedxml.cElementTree as ET
+
+TestSuite = collections.namedtuple('TestSuite', [
+    'cases', 'errors', 'failures', 'skips', 'collected', 'timetxt',
+])
 
 
 def process_junit_xml(filepath):
@@ -16,47 +22,49 @@ def process_junit_xml(filepath):
         failures = int(suite.attrib.get('failures', '0'))
         skips = int(suite.attrib.get('skips', '0'))
         collected = int(suite.attrib.get('tests', '0'))
-        timetxt = suite.attrib.get('time', '')
-        return _process_junit_cases(
+        timetxt = '{0} seconds'.format(
+            suite.attrib.get('time', '')
+        )
+        suiteobj = TestSuite(
             cases, errors, failures, skips, collected, timetxt,
         )
+        return _process_junit_cases(suiteobj)
     raise Exception('Failed to process JUnit XML')
 
 
-def _process_junit_cases(cases, errors, failures, skips, collected, timetxt):
+def _process_junit_cases(suite):
     """
     Produce Markdown from provided case details
     """
-    if errors == 0 and failures == 0:
-        if collected == 0:
-            return _no_tests_collected(timetxt)
-        return _all_tests_passed(collected, skips, timetxt)
-    return _test_issues(cases, errors, failures, skips, collected, timetxt)
+    if suite.errors == 0 and suite.failures == 0:
+        if suite.collected == 0:
+            return _no_tests_collected(suite)
+        return _all_tests_passed(suite)
+    return _test_issues(suite)
 
 
-def _no_tests_collected(timetxt):
+def _no_tests_collected(suite):
     """
     Report for empty results
     """
-    return 'No test cases found to run (ran in {0} seconds).'.format(
-        timetxt,
+    return 'No test cases found to run (ran in {0}).'.format(
+        suite.timetxt,
     )
 
 
-def _all_tests_passed(collected, skips, timetxt):
+def _all_tests_passed(suite):
     """
     Report for no failures or errors.
     """
     return (
         'All tests passed ({0} tests collected, {1} tests skipped, ran in'
-        ' {2} seconds).'.format(
-            collected, skips, timetxt,
+        ' {2}).'.format(
+            suite.collected, suite.skips, suite.timetxt,
         )
     )
 
 
-def _test_issues(  # pylint:disable=unused-argument
-                 cases, errors, failures, skips, collected, timetxt):
+def _test_issues(suite):  # pylint:disable=unused-argument
     """
     Report details of issues found
     """
