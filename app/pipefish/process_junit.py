385b18ac831bfb8a4c12d5fab71b5653ef6d0047
Tim Gates <tim.gates@iress.com>
2019-07-07 07:33:06 +1000

    Fix more testing

---
+++ app/pipefish/process_junit.py
@@ -0,0 +1,59 @@
+"""
+Utility functions for processing JUnit XML reports
+"""
+
+import defusedxml.cElementTree as ET
+
+
+def process_junit_xml(filepath):
+    """
+    Converts a JUnit XML file into a Markdown Report.
+    """
+    doc = ET.parse(filepath)
+    for suite in doc.iter('testsuite'):
+        cases = list(suite.iter('testcase'))
+        errors = int(suite.attrib.get('errors', '0'))
+        failures = int(suite.attrib.get('failures', '0'))
+        skips = int(suite.attrib.get('skips', '0'))
+        collected = int(suite.attrib.get('tests', '0'))
+        timetxt = suite.attrib.get('time', '')
+        return _process_junit_cases(
+            cases, errors, failures, skips, collected, timetxt,
+        )
+    raise Exception('Failed to process JUnit XML')
+
+
+def _process_junit_cases(cases, errors, failures, skips, collected, timetxt):
+    """
+    Produce Markdown from provided case details
+    """
+    if errors == 0 and failures == 0:
+        if collected == 0:
+            return _no_tests_collected(timetxt)
+        return _all_tests_passed(collected, skips, timetxt)
+    return _test_issues(cases, errors, failures, skips, collected, timetxt)
+
+
+def _no_tests_collected(timetxt):
+    """
+    Report for empty results
+    """
+    return 'no tests'
+
+
+def _all_tests_passed(collected, skips, timetxt):
+    """
+    Report for no failures or errors.
+    """
+    return (
+        'All tests passed ({0} tests collected, {1} tests skipped, ran in'
+        ' {3} seconds).'.format(
+            collected, skips, timetxt,
+        )
+    )
+
+
+def _test_issues(cases, errors, failures, skips, collected, timetxt):
+    """
+    """
+    return 'issues'
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -2,15 +2,23 @@
 Test modules for pipefish __main__
 """
 
+import pytest
+
 
 def test_main():
     """
     GIVEN the pipefish.__main__ module entry point WHEN calling
-    main THEN the call executes successfully with a result of `None`
+    main without correct arguments THEN the call raises a SystemExit with an
+    return code of 1
     """
     # Setup
     from pipefish.__main__ import main
-    # Exercise
-    result = main()  # pylint: disable=assignment-from-no-return
+    import mock
+    fake_docopt = mock.patch(
+        'pipefish.__main__.docopt', return_value={}
+    )
+    with fake_docopt, pytest.raises(SystemExit) as excctxt:
+        # Exercise
+        main()  # pylint: disable=assignment-from-no-return
     # Verify
-    assert result is None  # nosec
+    assert excctxt.value.args[0] == 1  # nosec
