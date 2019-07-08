9396918f73b460678e854b446931fc8a6d63ac7f
Tim Gates <tim.gates@iress.com>
2019-07-08 14:40:52 +1000

    Export additional function

--- app/pipefish/__init__.py
+++ app/pipefish/__init__.py
@@ -3,7 +3,11 @@ Utility functions for processing JUnit XML reports and
 Cobertura coverage XML reports into Markdown
 """
 
-from .process_cobertura import process_cobertura_xml
+from .process_cobertura import (get_coverage_from_cobertura_xml,
+                                process_cobertura_xml)
 from .process_junit import process_junit_xml
 
-__all__ = ['process_junit_xml', 'process_cobertura_xml']
+__all__ = [
+    'process_junit_xml', 'process_cobertura_xml',
+    'get_coverage_from_cobertura_xml',
+]
